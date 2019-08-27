634c7e60967fc47e6915b58d89fcdd9b5410f926
VEGALUISJ <Luisvega.jlvv@gmail.com>
2019-08-27 17:09:43 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
 
 import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let figureup = ["&#9829", "&#9830"];
+
+  let arriba = figureup[Math.floor(Math.random() * figureup.length)];
+
+  console.log("arriba");
 };
